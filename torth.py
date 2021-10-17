86c41a610f180b803be63c3916ccc8becdb6b177
CyberPaddy <teemu@paddy.fi>
2021-10-17 21:03:33 +0300

    Fixed exe_file when code file is not in the same directory

--- torth.py
+++ torth.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 from utils.lex import get_tokens_from_code
 from utils.program import compile_code, run_code
 from utils.util import get_code_file_from_arguments, usage
@@ -7,7 +8,9 @@ def main():
     code_file = get_code_file_from_arguments()
     tokens = get_tokens_from_code(code_file)
     compile_code(tokens)
-    run_code(code_file.replace(".torth", ""))
+
+    exe_file = os.path.basename(code_file).replace('.torth', '')
+    run_code(exe_file)
 
 if __name__ == "__main__":
     main()
