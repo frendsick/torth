0f7544777ac521f60a63a4096db24c887ea16ba4
Teemu Pätsi <teemu@paddy.fi>
2021-10-18 16:27:01 +0300

    Changed with open() as f -syntax to f = open() syntax
    This gives more control when the file should be closed when for example
    adding stuff to the middle of the assembly file

--- utils/asm.py
+++ utils/asm.py
@@ -52,163 +52,184 @@ def generate_arithmetic_asm(asm_file, operand: str) -> None:
     asm_file.write( '  push rax\n')
 
 def generate_asm(program: Program, asm_file: str) -> None:
-    with open (asm_file, 'a') as f:
-        for op in program:
-            # IF is just a keyword which does nothing
-            if op.type == OpType.IF:
+    for op in program:
+        f = open(asm_file, 'a')
+        # IF is just a keyword which does nothing
+        if op.type == OpType.IF:
+            f.write(get_op_comment_asm(op, op.type))
+        elif op.type == OpType.PUSH_INT:
+            f.write(get_op_comment_asm(op, op.type))
+            f.write(f'  mov rax, {op.token.value}\n')
+            f.write(f'  push rax\n')
+        elif op.type == OpType.PUSH_STR:
+            str_val = op.token.value[1:-1]  # Take quotes out of the string
+            str_len = len(op.token.value)-1 # String length without quotes plus newline
+
+            # Close the assembly file so the file can be rewritten in add_string_variable_asm
+            f.close()
+            add_string_variable_asm(asm_file, str_val)
+
+            f = open(asm_file, 'a')
+            f.write(get_op_comment_asm(op, op.type))
+            f.write(f'  mov rsi, str{STRING_COUNT} ; Pointer to string\n')
+            f.write( '  push rsi\n')
+ 
+        elif op.type == OpType.INTRINSIC:
+            intrinsic = op.token.value.upper()
+            # Push argument count to the stack
+            if intrinsic == "ARGC":
                 f.write(get_op_comment_asm(op, op.type))
-            elif op.type == OpType.PUSH_INT:
+                f.write(f'  push {len(sys.argv)}\n')
+            # Pop one element off the stack
+            elif intrinsic == "DROP":
                 f.write(get_op_comment_asm(op, op.type))
-                f.write(f'  mov rax, {op.token.value}\n')
-                f.write(f'  push rax\n')
-            elif op.type == OpType.INTRINSIC:
-                intrinsic = op.token.value.upper()
-                # Push argument count to the stack
-                if intrinsic == "ARGC":
-                    f.write(get_op_comment_asm(op, op.type))
-                    f.write(f'  push {len(sys.argv)}\n')
-                # Pop one element off the stack
-                elif intrinsic == "DROP":
-                    f.write(get_op_comment_asm(op, op.type))
-                    f.write( '  pop rax\n')
-                # Duplicate the top element of the stack
-                elif intrinsic == "DUP":
-                    f.write(get_op_comment_asm(op, op.type))
-                    f.write( '  pop rax\n')
-                    f.write( '  push rax\n')
-                    f.write( '  push rax\n')
-                elif intrinsic == "EQ":
-                    f.write(get_op_comment_asm(op, op.type))
-                    generate_comparison_asm(f, "cmove")
-                elif intrinsic == "GE":
-                    f.write(get_op_comment_asm(op, op.type))
-                    generate_comparison_asm(f, "cmovge")
-                elif intrinsic == "GT":
-                    f.write(get_op_comment_asm(op, op.type))
-                    generate_comparison_asm(f, "cmovgt")
-                elif intrinsic == "LE":
-                    f.write(get_op_comment_asm(op, op.type))
-                    generate_comparison_asm(f, "cmovle")
-                elif intrinsic == "LT":
-                    f.write(get_op_comment_asm(op, op.type))
-                    generate_comparison_asm(f, "cmovl")
-                elif intrinsic == "NE":
-                    f.write(get_op_comment_asm(op, op.type))
-                    generate_comparison_asm(f, "cmovne")
-                elif intrinsic == "OVER":
-                    f.write(get_op_comment_asm(op, op.type))
-                    f.write( '  pop rax\n')
-                    f.write( '  pop rbx\n')
-                    f.write( '  push rbx\n')
-                    f.write( '  push rax\n')
-                    f.write( '  push rbx\n')
-                elif intrinsic == "PLUS":
-                    f.write(get_op_comment_asm(op, op.type))
-                    generate_arithmetic_asm(f, "add")
-                elif intrinsic == "MINUS":
-                    f.write(get_op_comment_asm(op, op.type))
-                    generate_arithmetic_asm(f, "sub")
-                elif intrinsic == "MUL":
-                    f.write(get_op_comment_asm(op, op.type))
-                    f.write( '  pop rax\n')
-                    f.write( '  pop rbx\n')
-                    f.write( '  mul rbx\n')
-                    f.write( '  push rax\n')
-                # Rotate three top elements in the stack
-                elif intrinsic == "ROT":
-                    f.write(get_op_comment_asm(op, op.type))
-                    f.write( '  pop rax\n')
-                    f.write( '  pop rbx\n')
-                    f.write( '  pop rcx\n')
-                    f.write( '  push rbx\n')
-                    f.write( '  push rax\n')
-                    f.write( '  push rcx\n')
-                # Push remainder and quotient to stack
-                elif intrinsic == "DIVMOD":
-                    f.write(get_op_comment_asm(op, op.type))
-                    f.write( '  pop rax\n')
-                    f.write( '  pop rbx\n')
-                    f.write( '  xor rdx, rdx\n')
-                    f.write( '  div rbx\n')
-                    f.write( '  push rdx ; Remainder\n')
-                    f.write( '  push rax ; Quotient\n')
-                elif intrinsic == "PRINT_INT":
-                    f.write(get_op_comment_asm(op, op.type))
-                    f.write( '  mov [int], rsi\n')
-                    f.write( '  call PrintInt\n')
-                # Swap two elements at the top of the stack
-                elif intrinsic == "SWAP":
-                    f.write(get_op_comment_asm(op, op.type))
-                    f.write( '  pop rax\n')
-                    f.write( '  pop rbx\n')
-                    f.write( '  push rax\n')
-                    f.write( '  push rbx\n')
-                elif intrinsic == "SYSCALL0":
-                    f.write(get_op_comment_asm(op, op.type))
-                    f.write("  pop rax\n")
-                    f.write("  syscall\n")
-                    f.write("  push rax\n")
-                elif intrinsic == "SYSCALL1":
-                    f.write(get_op_comment_asm(op, op.type))
-                    f.write("  pop rax\n")
-                    f.write("  pop rdi\n")
-                    f.write("  syscall\n")
-                    f.write("  push rax\n")
-                elif intrinsic == "SYSCALL2":
-                    f.write(get_op_comment_asm(op, op.type))
-                    f.write("  pop rax\n");
-                    f.write("  pop rdi\n");
-                    f.write("  pop rsi\n");
-                    f.write("  syscall\n");
-                    f.write("  push rax\n")
-                elif intrinsic == "SYSCALL3":
-                    f.write(get_op_comment_asm(op, op.type))
-                    f.write("  pop rax\n")
-                    f.write("  pop rdi\n")
-                    f.write("  pop rsi\n")
-                    f.write("  pop rdx\n")
-                    f.write("  syscall\n")
-                    f.write("  push rax\n")
-                elif intrinsic == "SYSCALL4":
-                    f.write(get_op_comment_asm(op, op.type))
-                    f.write("  pop rax\n")
-                    f.write("  pop rdi\n")
-                    f.write("  pop rsi\n")
-                    f.write("  pop rdx\n")
-                    f.write("  pop r10\n")
-                    f.write("  syscall\n")
-                    f.write("  push rax\n")
-                elif intrinsic == "SYSCALL5":
-                    f.write(get_op_comment_asm(op, op.type))
-                    f.write("  pop rax\n")
-                    f.write("  pop rdi\n")
-                    f.write("  pop rsi\n")
-                    f.write("  pop rdx\n")
-                    f.write("  pop r10\n")
-                    f.write("  pop r8\n")
-                    f.write("  syscall\n")
-                    f.write("  push rax\n")
-                elif intrinsic == "SYSCALL6":
-                    f.write(get_op_comment_asm(op, op.type))
-                    f.write("  pop rax\n")
-                    f.write("  pop rdi\n")
-                    f.write("  pop rsi\n")
-                    f.write("  pop rdx\n")
-                    f.write("  pop r10\n")
-                    f.write("  pop r8\n")
-                    f.write("  pop r9\n")
-                    f.write("  syscall\n")
-                    f.write("  push rax\n")
-                else:
-                    raise NotImplementedError(f"Intrinsic '{op.token.value}' is not implemented")
+                f.write( '  pop rax\n')
+            # Duplicate the top element of the stack
+            elif intrinsic == "DUP":
+                f.write(get_op_comment_asm(op, op.type))
+                f.write( '  pop rax\n')
+                f.write( '  push rax\n')
+                f.write( '  push rax\n')
+            elif intrinsic == "EQ":
+                f.write(get_op_comment_asm(op, op.type))
+                generate_comparison_asm(f, "cmove")
+            elif intrinsic == "GE":
+                f.write(get_op_comment_asm(op, op.type))
+                generate_comparison_asm(f, "cmovge")
+            elif intrinsic == "GT":
+                f.write(get_op_comment_asm(op, op.type))
+                generate_comparison_asm(f, "cmovgt")
+            elif intrinsic == "LE":
+                f.write(get_op_comment_asm(op, op.type))
+                generate_comparison_asm(f, "cmovle")
+            elif intrinsic == "LT":
+                f.write(get_op_comment_asm(op, op.type))
+                generate_comparison_asm(f, "cmovl")
+            elif intrinsic == "NE":
+                f.write(get_op_comment_asm(op, op.type))
+                generate_comparison_asm(f, "cmovne")
+            elif intrinsic == "OVER":
+                f.write(get_op_comment_asm(op, op.type))
+                f.write( '  pop rax\n')
+                f.write( '  pop rbx\n')
+                f.write( '  push rbx\n')
+                f.write( '  push rax\n')
+                f.write( '  push rbx\n')
+            elif intrinsic == "PLUS":
+                f.write(get_op_comment_asm(op, op.type))
+                generate_arithmetic_asm(f, "add")
+            elif intrinsic == "MINUS":
+                f.write(get_op_comment_asm(op, op.type))
+                generate_arithmetic_asm(f, "sub")
+            elif intrinsic == "MUL":
+                f.write(get_op_comment_asm(op, op.type))
+                f.write( '  pop rax\n')
+                f.write( '  pop rbx\n')
+                f.write( '  mul rbx\n')
+                f.write( '  push rax\n')
+            # Rotate three top elements in the stack
+            elif intrinsic == "ROT":
+                f.write(get_op_comment_asm(op, op.type))
+                f.write( '  pop rax\n')
+                f.write( '  pop rbx\n')
+                f.write( '  pop rcx\n')
+                f.write( '  push rbx\n')
+                f.write( '  push rax\n')
+                f.write( '  push rcx\n')
+            # Push remainder and quotient to stack
+            elif intrinsic == "DIVMOD":
+                f.write(get_op_comment_asm(op, op.type))
+                f.write( '  pop rax\n')
+                f.write( '  pop rbx\n')
+                f.write( '  xor rdx, rdx\n')
+                f.write( '  div rbx\n')
+                f.write( '  push rdx ; Remainder\n')
+                f.write( '  push rax ; Quotient\n')
+            elif intrinsic == "PRINT":
+                f.write(get_op_comment_asm(op, op.type))
+                f.write( '  mov rax, 1 ; write syscall\n')
+                f.write( '  mov rdi, 1 ; fd 1 => stdout\n')
+                f.write( '  mov rsi, [rsp] ; *buf\n')
+                f.write(f'  mov rdx, lenStr{STRING_COUNT} ; count\n')
+                f.write( '  syscall\n')
+                f.write( '  add rsp, 16 ; drop two values from the stack\n')
+                f.write( '  push rax ; return code\n')
+            elif intrinsic == "PRINT_INT":
+                f.write(get_op_comment_asm(op, op.type))
+                f.write( '  mov [int], rsi\n')
+                f.write( '  call PrintInt\n')
+            # Swap two elements at the top of the stack
+            elif intrinsic == "SWAP":
+                f.write(get_op_comment_asm(op, op.type))
+                f.write( '  pop rax\n')
+                f.write( '  pop rbx\n')
+                f.write( '  push rax\n')
+                f.write( '  push rbx\n')
+            elif intrinsic == "SYSCALL0":
+                f.write(get_op_comment_asm(op, op.type))
+                f.write("  pop rax\n")
+                f.write("  syscall\n")
+                f.write("  push rax\n")
+            elif intrinsic == "SYSCALL1":
+                f.write(get_op_comment_asm(op, op.type))
+                f.write("  pop rax\n")
+                f.write("  pop rdi\n")
+                f.write("  syscall\n")
+                f.write("  push rax\n")
+            elif intrinsic == "SYSCALL2":
+                f.write(get_op_comment_asm(op, op.type))
+                f.write("  pop rax\n");
+                f.write("  pop rdi\n");
+                f.write("  pop rsi\n");
+                f.write("  syscall\n");
+                f.write("  push rax\n")
+            elif intrinsic == "SYSCALL3":
+                f.write(get_op_comment_asm(op, op.type))
+                f.write("  pop rax\n")
+                f.write("  pop rdi\n")
+                f.write("  pop rsi\n")
+                f.write("  pop rdx\n")
+                f.write("  syscall\n")
+                f.write("  push rax\n")
+            elif intrinsic == "SYSCALL4":
+                f.write(get_op_comment_asm(op, op.type))
+                f.write("  pop rax\n")
+                f.write("  pop rdi\n")
+                f.write("  pop rsi\n")
+                f.write("  pop rdx\n")
+                f.write("  pop r10\n")
+                f.write("  syscall\n")
+                f.write("  push rax\n")
+            elif intrinsic == "SYSCALL5":
+                f.write(get_op_comment_asm(op, op.type))
+                f.write("  pop rax\n")
+                f.write("  pop rdi\n")
+                f.write("  pop rsi\n")
+                f.write("  pop rdx\n")
+                f.write("  pop r10\n")
+                f.write("  pop r8\n")
+                f.write("  syscall\n")
+                f.write("  push rax\n")
+            elif intrinsic == "SYSCALL6":
+                f.write(get_op_comment_asm(op, op.type))
+                f.write("  pop rax\n")
+                f.write("  pop rdi\n")
+                f.write("  pop rsi\n")
+                f.write("  pop rdx\n")
+                f.write("  pop r10\n")
+                f.write("  pop r8\n")
+                f.write("  pop r9\n")
+                f.write("  syscall\n")
+                f.write("  push rax\n")
             else:
                 raise NotImplementedError(f"Operand '{op.type}' is not implemented")
 
-        # Add exit syscall to asm_file
-        f.write( ';; -- exit syscall\n')
-        f.write( '  mov rax, 0x1\n')
-        f.write( '  xor rbx, rbx\n')
-        f.write( '  int 0x80\n')
+    # Add exit syscall to asm_file
+    f.write( ';; -- exit syscall\n')
+    f.write( '  mov rax, 0x1\n')
+    f.write( '  xor rbx, rbx\n')
+    f.write( '  int 0x80\n')
+    f.close()
 
 def compile_asm(asm_file: str) -> None:
     subprocess.run(['nasm', '-felf64', f'-o{asm_file.replace(".asm", ".o")}', asm_file])
