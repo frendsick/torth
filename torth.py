8740acdaec6985481a1187db40f5af15ed258da7
CyberPaddy <teemu@paddy.fi>
2021-10-27 20:47:21 +0300

    Continue implementing command line arguments

--- torth.py
+++ torth.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 import os
 from utils.lex import get_tokens_from_code
-from utils.program import compile_code, run_code
-from utils.util import get_code_file_from_arguments
+from utils.program import compile_code, remove_compilation_files, run_code
+from utils.util import get_command_line_arguments
 
 def main():
-    code_file = get_code_file_from_arguments()
-    tokens = get_tokens_from_code(code_file)
-    compile_code(tokens)
+    args = get_command_line_arguments()
+    tokens = get_tokens_from_code(args.code_file)
+    code_file_basename = os.path.basename(args.code_file)
 
-    exe_file = os.path.basename(code_file).replace('.torth', '')
-    run_code(exe_file)
+    if args.output is not None:
+        exe_file = args.output
+    else:
+        exe_file = code_file_basename.replace('.torth', '')
+
+    compile_code(tokens, code_file_basename, exe_file)
+    remove_compilation_files(code_file_basename, args)
+    if args.run:
+        run_code(exe_file)
 
 if __name__ == "__main__":
     main()
