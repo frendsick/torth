eb36659171f8e1b14e271199b027edbf9500c8e2
CyberPaddy <teemu@paddy.fi>
2022-05-02 23:20:39 +0300

    Modified ArgumentParser

--- utils/util.py
+++ utils/util.py
@@ -7,13 +7,13 @@ def usage() -> None:
     exit(1)
 
 def get_command_line_arguments() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(usage=f'{sys.argv[0]} [options] code_file', description='Compile Torth code')
-    parser.add_argument('--output', '-o', help='Output file')
-    parser.add_argument('--run', '-r', action='store_true', help='Run program after compilation')
-    parser.add_argument('--save-asm', action='store_true', help='Save assembly file as <code_file>.asm')
+    parser = argparse.ArgumentParser(description='Compile Torth code')
+    parser.add_argument('--output', '-o', help='Output file', metavar='file')
+    parser.add_argument('-r', '--run', action='store_true', help='Run program after compilation')
+    parser.add_argument('-s', '--save-asm', action='store_true', help='Save assembly file as <code_file>.asm')
     parser.add_argument('code_file', help='Input file')
 
     args: argparse.Namespace = parser.parse_args(sys.argv[1:])
     if not os.path.isfile(args.code_file):
-        FileNotFoundError(f"Argument '{sys.argv[1]}' is not a file")
+        raise FileNotFoundError(f"Argument '{args.code_file}' is not a file")
     return args
