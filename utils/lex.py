c3fecc41a7fcaa102f60ff56b73e51acdd131260
CyberPaddy <teemu@paddy.fi>
2022-05-03 14:53:47 +0300

    Refactored the order of functions

--- utils/lex.py
+++ utils/lex.py
@@ -3,6 +3,42 @@ import os
 from typing import List
 from utils.defs import Keyword, TokenType, Location, Token
 
+def get_tokens_from_code(file: str, code: str) -> List[Token]:
+    tokens: List[Token] = []
+    token_matches: List[re.Match[str]] = get_token_matches(code)
+
+    # Newlines are used to determine when a comment ends and when new line starts
+    newline_indexes: List[int]  = [nl.start() for nl in re.finditer('\n', code)]
+
+    for match in token_matches:
+        token = get_token_from_match(match, os.path.basename(file), newline_indexes)
+        tokens.append(token)
+
+    return tokens
+
+# Returns all tokens with comments taken out
+def get_token_matches(code: str) -> List[re.Match[str]]:
+    TOKEN_REGEX: re.Pattern[str] = re.compile(r'''\[.*\]|".*?"|'.*?'|\S+''')
+    matches: List[re.Match[str]] = list(re.finditer(TOKEN_REGEX, code))
+    code_without_comments: str = re.sub(r'\s*\/\/.*', '', code)
+    matches_without_comments: List[re.Match[str]] = list(re.finditer(TOKEN_REGEX, code_without_comments))
+
+    # Take comments out of matches
+    i: int = 0
+    while i < len(matches):
+        if matches[i].group(0) != matches_without_comments[i].group(0):
+            matches.pop(i)
+            i -= 1
+        i += 1
+    return matches
+
+# Constructs and returns a Token object from a regex match
+def get_token_from_match(match: re.Match[str], file: str, newline_indexes: List[int]) -> Token:
+    token_value     = get_token_value(match.group(0))
+    token_type      = get_token_type(token_value)
+    token_location  = get_token_location(file, match.start(), newline_indexes)
+    return Token(token_value, token_type, token_location)
+
 # Returns the Intrinsic class value from token
 def get_token_value(token: str) -> str:
     if token == '%':
@@ -72,39 +108,3 @@ def get_token_location(filename: str, position: int, newline_indexes: List[int])
         row += 1
         col = position - newline_indexes[-1] - 1
     return (filename, row, col+1)
-
-# Returns all tokens with comments taken out
-def get_token_matches(code: str) -> List[re.Match[str]]:
-    TOKEN_REGEX: re.Pattern[str] = re.compile(r'''\[.*\]|".*?"|'.*?'|\S+''')
-    matches: List[re.Match[str]] = list(re.finditer(TOKEN_REGEX, code))
-    code_without_comments: str = re.sub(r'\s*\/\/.*', '', code)
-    matches_without_comments: List[re.Match[str]] = list(re.finditer(TOKEN_REGEX, code_without_comments))
-
-    # Take comments out of matches
-    i: int = 0
-    while i < len(matches):
-        if matches[i].group(0) != matches_without_comments[i].group(0):
-            matches.pop(i)
-            i -= 1
-        i += 1
-    return matches
-
-# Constructs and returns a Token object from a regex match
-def get_token_from_match(match: re.Match[str], file: str, newline_indexes: List[int]) -> Token:
-    token_value     = get_token_value(match.group(0))
-    token_type      = get_token_type(token_value)
-    token_location  = get_token_location(file, match.start(), newline_indexes)
-    return Token(token_value, token_type, token_location)
-
-def get_tokens_from_code(file: str, code: str) -> List[Token]:
-    tokens: List[Token] = []
-    token_matches: List[re.Match[str]] = get_token_matches(code)
-
-    # Newlines are used to determine when a comment ends and when new line starts
-    newline_indexes: List[int]  = [nl.start() for nl in re.finditer('\n', code)]
-
-    for match in token_matches:
-        token = get_token_from_match(match, os.path.basename(file), newline_indexes)
-        tokens.append(token)
-
-    return tokens
