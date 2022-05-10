8f65e7840b4db3e312fec42584c9ea2c1d17f410
CyberPaddy <teemu@paddy.fi>
2022-05-10 17:59:33 +0300

    Refactor torth.py

--- torth.py
+++ torth.py
@@ -3,14 +3,14 @@ import argparse
 import os
 from typing import List
 from compiler.compile import compile_code, remove_compilation_files
-from compiler.defs import Function, Token
-from compiler.lex import get_functions_from_code, get_included_files, get_tokens_from_functions
+from compiler.defs import Function
+from compiler.lex import get_functions_from_code, get_included_files
 from compiler.program import run_code
 from compiler.utils import get_command_line_arguments, get_file_contents
 
 def main():
-    args: argparse.Namespace = get_command_line_arguments()
-    code: str = get_file_contents(args.code_file)
+    args: argparse.Namespace    = get_command_line_arguments()
+    code: str                   = get_file_contents(args.code_file)
     included_files: List[str]   = get_included_files(code)
     functions: List[Function]   = get_functions_from_code(code, args.code_file, included_files)
 
