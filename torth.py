dc2f89a68682877f67abf112b3b1193498550692
Teemu Pätsi <teemu@paddy.fi>
2022-05-17 17:41:46 +0300

    Fixed changed function signatures

--- torth.py
+++ torth.py
@@ -7,7 +7,7 @@ import os
 from typing import List
 from compiler.compile import compile_code, remove_compilation_files
 from compiler.defs import Constant, Function, Memory
-from compiler.lex import get_constants_from_functions, get_functions_from_code
+from compiler.lex import get_constants_from_functions, get_functions_from_files
 from compiler.lex import get_included_files, get_memories_from_code
 from compiler.program import run_code
 from compiler.utils import get_command_line_arguments, get_file_contents
@@ -17,9 +17,9 @@ def main():
     args: argparse.Namespace    = get_command_line_arguments()
     code: str                   = get_file_contents(args.code_file)
     included_files: List[str]   = get_included_files(code)
-    functions: List[Function]   = get_functions_from_code(args.code_file, included_files)
+    functions: List[Function]   = get_functions_from_files(args.code_file, included_files)
     constants: List[Constant]   = get_constants_from_functions(functions)
-    memories: List[Memory]      = get_memories_from_code(args.code_file, included_files, constants)
+    memories: List[Memory]      = get_memories_from_code(included_files, constants)
 
     # Executable's file name is code file name without extension by default
     code_file_basename: str = os.path.basename(args.code_file)
