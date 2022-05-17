d96c769240b7a89d81afae48db0ad49cf70734b5
Teemu Pätsi <teemu@paddy.fi>
2022-05-17 16:26:09 +0300

    Linted defs.py with pylint

--- compiler/defs.py
+++ compiler/defs.py
@@ -1,16 +1,20 @@
+"""
+Definitions for classes, constants, and types used by the Torth compiler
+"""
 from dataclasses import dataclass
 from enum import Enum, auto
 from typing import Dict, List, Tuple
 
 STACK: List[str] = []
-
-class Colors:
-    FAIL    = '\033[91m'
-    HEADER  = '\033[95m'
-    NC      = '\033[0m'
-    WARNING = '\033[93m'
+COLORS: Dict[str, str] = {
+    'FAIL'      : '\033[91m',
+    'HEADER'    : '\033[95m',
+    'NC'        : '\033[0m',
+    'WARNING'   : '\033[93m'
+}
 
 class Keyword(Enum):
+    """Available keywords in the Torth language"""
     BREAK=auto()
     CONST=auto()
     DO=auto()
@@ -25,6 +29,7 @@ class Keyword(Enum):
     WHILE=auto()
 
 class Intrinsic(Enum):
+    """Available intrinsics in the Torth language"""
     AND=auto()
     ARGC=auto()
     ARGV=auto()
@@ -66,6 +71,7 @@ class Intrinsic(Enum):
     SYSCALL6=auto()
 
 class OpType(Enum):
+    """Available operand types in the Torth language"""
     BREAK=auto()
     DO=auto()
     DONE=auto()
@@ -84,6 +90,7 @@ class OpType(Enum):
     WHILE=auto()
 
 class TokenType(Enum):
+    """Available Types for Token objects"""
     ARRAY=auto()
     BOOL=auto()
     CHAR=auto()
@@ -99,12 +106,14 @@ Memory      = Tuple[str, str, Location] # Name, str(size), Location
 
 @dataclass
 class Constant:
+    """CONST keyword can be used to create a named immutable integer value"""
     name: str
     value: str
     location: Location
 
 @dataclass
 class Token:
+    """Tokens are words from a program that translates to certain operands"""
     value: str
     type: TokenType
     location: Location
@@ -114,19 +123,21 @@ Signature   = Tuple[List[str], List[str]]
 
 @dataclass
 class Function:
+    """Functions are reusable named sequences of Token objects"""
     name: str
     signature: Signature
     tokens: List[Token]
 
 @dataclass
 class Op:
+    """Operands stores Token's information which is used in assembly code generation"""
     id: int
     type: OpType
     token: Token
 
 Program = List[Op]
 TYPE_REGEX: Dict[str, str] = {
-    'INT': '-?\d+',
-    'PTR': '\*ptr \S+',
-    'STR': '\*buf s_\S+'
+    'INT': r'-?\d+',
+    'PTR': r'\*ptr \S+',
+    'STR': r'\*buf s_\S+'
 }
--- compiler/utils.py
+++ compiler/utils.py
@@ -3,7 +3,7 @@ import os
 import re
 import sys
 from typing import NoReturn, Optional
-from compiler.defs import Colors, Token, TYPE_REGEX
+from compiler.defs import COLORS, Token, TYPE_REGEX
 
 def usage() -> NoReturn:
     print("Usage: ./torth.py file")
@@ -26,24 +26,24 @@ def get_file_contents(file: str) -> str:
         return f.read()
 
 def compiler_error(error_type: str, error_message: str, token: Optional[Token] = None) -> NoReturn:
-    print(f'{Colors.HEADER}Compiler error {Colors.FAIL}{error_type}{Colors.NC}' + f":\n{error_message}")
+    print(f"{COLORS['HEADER']}Compiler error {COLORS['FAIL']}{error_type}{COLORS['NC']}" + f":\n{error_message}")
     if token:
         print(get_token_location_info(token))
     exit(1)
 
 def get_token_location_info(token: Token) -> str:
     return f'''
-{Colors.HEADER}Operand{Colors.NC}: {token.value}
-{Colors.HEADER}File{Colors.NC}: {token.location[0]}
-{Colors.HEADER}Row{Colors.NC}: {token.location[1]}
-{Colors.HEADER}Column{Colors.NC}: {token.location[2]}'''
+{COLORS['HEADER']}Operand{COLORS['NC']}: {token.value}
+{COLORS['HEADER']}File{COLORS['NC']}: {token.location[0]}
+{COLORS['HEADER']}Row{COLORS['NC']}: {token.location[1]}
+{COLORS['HEADER']}Column{COLORS['NC']}: {token.location[2]}'''
 
 def check_popped_value_type(token: Token, popped_value: str, expected_type: str) -> None:
     regex: str = TYPE_REGEX[expected_type]
     error_message: str = f"Wrong type of value popped from the stack.\n\n" + \
-        f"{Colors.HEADER}Value{Colors.NC}: {popped_value}\n" + \
-        f"{Colors.HEADER}Expected{Colors.NC}: {expected_type}\n" + \
-        f"{Colors.HEADER}Regex{Colors.NC}: {regex}"
+        f"{COLORS['HEADER']}Value{COLORS['NC']}: {popped_value}\n" + \
+        f"{COLORS['HEADER']}Expected{COLORS['NC']}: {expected_type}\n" + \
+        f"{COLORS['HEADER']}Regex{COLORS['NC']}: {regex}"
 
     # Raise compiler error if the value gotten from the stack does not match with the regex
     assert re.match(regex, popped_value), compiler_error("REGISTER_VALUE_ERROR", error_message, token)
