d37b84933f251fd02fa525afdedf2d689b4be04d
Teemu Pätsi <teemu@paddy.fi>
2022-06-01 02:37:25 +0300

    Do not exit if errors occur during tests

--- tests/compile_all.sh
+++ tests/compile_all.sh
@@ -2,10 +2,9 @@
 shopt -s globstar
 script_dir="$(dirname -- "$(realpath -- "${BASH_SOURCE[0]}")")"
 for example_file in $script_dir/../examples/**/*.torth; do
-    echo "Compiling $example_file"
+    echo "[INFO] Compiling $example_file"
     python3 $script_dir/../torth.py $example_file
     if [ $? -ne 0 ]; then
-        echo "Compiling $example_file failed"
-        exit 1
+        echo "[ERROR] Compiling $example_file failed"
     fi
 done
