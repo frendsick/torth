c26523a183337d945afa95905fb1551a384ba598
CyberPaddy <teemu@paddy.fi>
2022-07-15 03:19:03 +0300

    Included files are only included once

--- torth.py
+++ torth.py
@@ -4,7 +4,7 @@ Driver code for the Torth compiler
 """
 import argparse
 import os
-from typing import List
+from typing import List, Set
 from compiler.compile import compile_code, link_object_file, remove_compilation_files
 from compiler.defs import Constant, Function, Memory, Program, Token
 from compiler.lex import add_enums_to_constants, get_constants_from_functions, get_functions_from_files
@@ -18,8 +18,8 @@ def main():
     print_if_verbose(f"Parsing the code from {args.code_file}", args.verbose)
     code: str                   = get_file_contents(args.code_file)
     compiler_directory: str     = os.path.dirname(os.path.abspath(__file__))
-    included_files: List[str]   = get_included_files(code, compiler_directory, args.path)
-    included_files.append(args.code_file)
+    included_files: Set[str]    = get_included_files(code, compiler_directory, args.path)
+    included_files.add(args.code_file)
 
     functions: List[Function]   = get_functions_from_files(included_files)
     constants: List[Constant]   = get_constants_from_functions(functions)
