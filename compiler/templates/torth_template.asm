7f8750f29f5057f10e125d20701eadd9fe932fa4
CyberPaddy <teemu@paddy.fi>
2022-07-17 20:10:25 +0300

    Added Assembly template file

---
+++ compiler/templates/torth_template.asm
@@ -0,0 +1,47 @@
+default rel
+
+;; DEFINES
+%define sys_exit 60
+section .data
+
+section .bss
+  args_ptr: resq 1
+  return_stack: resb 1337*64
+  return_stack_index: resq 1
+
+section .text
+
+;; Joinked from Porth's print function, thank you Tsoding!
+print:
+  mov     r9, -3689348814741910323
+  sub     rsp, 40
+  lea     rcx, [rsp+30]
+.L2:
+  mov     rax, rdi
+  lea     r8, [rsp+32]
+  mul     r9
+  mov     rax, rdi
+  sub     r8, rcx
+  shr     rdx, 3
+  lea     rsi, [rdx+rdx*4]
+  add     rsi, rsi
+  sub     rax, rsi
+  add     eax, 48
+  mov     BYTE [rcx], al
+  mov     rax, rdi
+  mov     rdi, rdx
+  mov     rdx, rcx
+  sub     rcx, 1
+  cmp     rax, 9
+  ja      .L2
+  lea     rax, [rsp+32]
+  mov     edi, 1
+  sub     rdx, rax
+  xor     eax, eax
+  lea     rsi, [rsp+32+rdx]
+  dec     r8
+  mov     rdx, r8
+  mov     rax, 1
+  syscall
+  add     rsp, 40
+  ret
