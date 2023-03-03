0daf1d3ace90d4588f8f40e5c6664a31790a4d6a
CyberPaddy <teemu@paddy.fi>
2023-03-04 01:22:37 +0200

    Reading code from file

--- torth-rust/src/main.rs
+++ torth-rust/src/main.rs
@@ -2,11 +2,14 @@ mod class;
 mod data_types;
 mod intrinsics;
 
-fn main() {
+fn main() -> Result<(), std::io::Error>{
     // TODO: Parse command line arguments
-    // TODO: Read code file
+    // TODO: Get code file name from command line arguments
+    const code_file: &str = "test.torth";
+    let code: String = std::fs::read_to_string(code_file)?;
     // TODO: Perform lexical analysis
     // TODO: Type check the program
     // TODO: Generate Assembly
     // TODO: Compile the program
+    Ok(())
 }
