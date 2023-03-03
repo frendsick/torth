6fde72fc5d7281c68ea7b30c199d298df2d93dab
CyberPaddy <teemu@paddy.fi>
2023-03-04 00:42:34 +0200

    Add Function struct

---
+++ torth-rust/src/class/function.rs
@@ -0,0 +1,9 @@
+use super::signature::Signature;
+use super::token::Token;
+
+#[derive(Debug, Clone)]
+pub struct Function<'a> {
+    name: &'a str,
+    signature: Signature,
+    tokens: Vec<Token<'a>>,
+}
--- torth-rust/src/class/mod.rs
+++ torth-rust/src/class/mod.rs
@@ -1,3 +1,4 @@
+pub mod function;
 pub mod location;
 pub mod signature;
 pub mod token;
